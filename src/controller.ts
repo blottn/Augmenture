52ed1dcb30aa283ae3819540ccbece85ac783b65
Nicholas Blott <blottn@tcd.ie>
2019-09-30 19:49:17 +0100

    added listall endpoint

--- src/controller.ts
+++ src/controller.ts
@@ -1,7 +1,17 @@
+import CardModel from './models/card';
 import { Request, Response } from "express";
 
-function listAll(request: Request, response: Response) {
-    response.send('hello world\n');
+async function listAll(request: Request, response: Response) {
+    let output = '';
+    await CardModel.find(function (err, cards) {
+        if (err) return console.log(err);
+        cards.map((card) => {
+            output += JSON.stringify(card) + '\n';
+        });
+    });
+    response.send(output);
 }
+
+
 
 export {listAll};
