442025ce11854a64776d47bd811aad37e572548b
Nicholas Blott <blottn@tcd.ie>
2019-10-08 20:25:46 +0100

    fixed imports

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp, {series} = require('gulp');
+const gulp = require('gulp');
 const gulpTS = require('gulp-typescript')
 const project = gulpTS.createProject('tsconfig.json');
 
@@ -21,4 +21,4 @@ function move(callback) {
 exports.move = move;
 
 exports.build = build;
-exports.default = series(build, move);
+exports.default = gulp.series(build, move);
