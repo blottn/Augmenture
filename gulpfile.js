87ea7ae1b7ebcd865880d047526d24234e8eae36
blottn <blottn@tcd.ie>
2019-10-08 19:14:29 +0100

    added index

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,23 @@ const gulp = require('gulp');
 const gulpTS = require('gulp-typescript')
 const project = gulpTS.createProject('tsconfig.json');
 
+const outDir = 'dist';
 
 function build(callback) {
     project.src()
         .pipe(project())
-        .js.pipe(gulp.dest('dist'));
+        .js
+        .pipe(gulp.dest('dist'));
     callback();
 }
+
+function move(callback) {
+    gulp.src(['./static/**/*'], { base: './' })
+        .pipe(gulp.dest('dist'));
+    callback();
+}
+
+exports.move = move;
 
 exports.build = build;
 exports.default = build;
--- src/controller.ts
+++ src/controller.ts
@@ -1,8 +1,10 @@
+const path = require('path');
+
 import CardModel from './models/card';
 import { Request, Response } from "express";
 
 export async function index(request: Request, response: Response) {
+    response.sendFile(path.join(__dirname, 'static', 'index.html'));
 
-    response.send("OK");
 }
 
