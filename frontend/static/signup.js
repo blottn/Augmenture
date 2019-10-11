46bf1b0bd4833980b6662cfb70b8a5375ce802a4
Nicholas Blott <blottn@tcd.ie>
2019-10-11 22:00:37 +0100

    added cycling adjective

---
+++ frontend/static/signup.js
@@ -0,0 +1,32 @@
+const adjectives = [
+    'amazing',
+    'awesome',
+    'incredible',
+    'marvelous',
+    'colossal',
+    'fantastic',
+    'monumental',
+    'tremendous',
+    'astounding',
+    'wonderful',
+    'glorius',
+    'dazzling',
+    'sublime',
+    'superb',
+    'shining',
+    'ilustrious'
+];
+
+
+
+
+function newAdjective() {
+    let index = Math.floor(Math.random() * adjectives.length)
+    $(".adjective").text(adjectives[index] + " ");
+    setTimeout(newAdjective,3000);
+}
+
+$(document).ready(() => {
+        
+    newAdjective();
+});
