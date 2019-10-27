13408c41b036ce89adf5cf6744b39ba26ee37399
Nicholas Blott <blottn@tcd.ie>
2019-10-27 17:20:58 +0000

    linted header

--- src/frontend/header.tsx
+++ src/frontend/header.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
+import * as React from 'react';
 
-export class Header extends React.Component {
+export default (): React.Component => (
+    <>
+        {/* css */}
+        {/* bootstrap */}
+        <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        />
+        <link
+            rel="stylesheet"
+            href="http://localhost:3000/static/styles/index.css"
+        />
+        {/* js */}
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" />
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" />
 
-    render() {
-        return (
-            <>
-            {/* css */}
-            {/* bootstrap */}
-                <link rel="stylesheet"
-                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
-                <link rel="stylesheet"
-                    href="http://localhost:3000/static/styles/index.css" />
-            {/* js */}
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-                <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-
-            {/* react */}
-                <script src="https://unpkg.com/react@16/umd/react.development.js"></script>
-                <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
-                <script src="http://localhost:3000/static/bundle.js"></script>
-            </>
-        );
-    }
-
-}
+        {/* react */}
+        <script src="https://unpkg.com/react@16/umd/react.development.js" />
+        <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" />
+        <script src="http://localhost:3000/static/bundle.js" />
+    </>
+);
