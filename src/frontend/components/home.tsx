5a271549cba8ae02239a2fa261938e7f3ce645d9
Nicholas Blott <blottn@tcd.ie>
2019-11-14 00:15:30 +0000

    removed debug log, added keys react complained about

--- src/frontend/components/home.tsx
+++ src/frontend/components/home.tsx
@@ -16,13 +16,13 @@ class HomePage extends React.Component<{}, HomeProps> {
     constructor(props) {
         super(props);
         const initial = [];
+        
         for (let i = 0; i < 10; i += 1) {
             initial.push({
                 title: `Panel number ${i}`,
                 contents: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate malesuada arcu, nec placerat ipsum maximus at',
             });
         }
-        console.log(initial);
         this.state = {
             cards: initial,
         };
@@ -31,7 +31,7 @@ class HomePage extends React.Component<{}, HomeProps> {
     getCards(): JSX.Element[] {
         const { cards } = this.state;
         return cards.map(({ title, contents }) => (
-            <Card title={title} contents={contents} />
+            <Card title={title} contents={contents} key={title} />
         ));
     }
 
