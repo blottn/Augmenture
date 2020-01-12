7539659e69a69e15bfd8d8dad0bb92ae8044f567
Nicholas Blott <blottn@tcd.ie>
2020-01-12 22:10:18 +0000

    added logout button

--- src/frontend/components/nav.tsx
+++ src/frontend/components/nav.tsx
@@ -11,17 +11,22 @@ const withNav = <P extends {}>(Page):
     const nav = ({ user, ...rest }): JSX.Element => (
         <div className="flex flex-nowrap aug-nav-root">
             <div className="flex flex-column aug-nav text-center">
-                <h2>
-                    <span className="text-augmenture">
-                        A
-                    </span>
-                </h2>
-                <p className="aug-nav-user">{ user }</p>
-                <hr className="aug-hr" />
-                <h5>Home</h5>
-                <h5>Recents</h5>
-                <h5>Starred</h5>
-                <h5>Some other utility</h5>
+                <div className="flex flex-column aug-nav-main">
+                    <h2>
+                        <span className="text-augmenture">
+                            A
+                        </span>
+                    </h2>
+                    <p className="aug-nav-user">{ user }</p>
+                    <hr className="aug-hr" />
+                    <p className="aug-nav-head">Home</p>
+                    <p className="aug-nav-head">Recents</p>
+                    <p className="aug-nav-head">Starred</p>
+                    <p className="aug-nav-head">Integrations</p>
+                </div>
+                <div className="aug-nav-footer">
+                    <button className="btn btn-outline-light">Log Out</button>
+                </div>
             </div>
             <div className="fill aug-nav-page">
                 <Page {...rest} />
